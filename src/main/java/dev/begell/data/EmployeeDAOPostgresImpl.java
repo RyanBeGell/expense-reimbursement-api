09f7d572b2d999a42cc9e65a9fc98c20a319a526
RyanBeGell <Ryanbegell@outlook.com>
2022-04-19 17:07:36 -0400

    EmployeeDAOPostresImpl and respective JUnit test class added

--- src/main/java/dev/begell/data/EmployeeDAOPostgresImpl.java
+++ src/main/java/dev/begell/data/EmployeeDAOPostgresImpl.java
@@ -1,21 +1,108 @@
 package dev.begell.data;
 
+import dev.begell.exceptions.ResourceNotFoundException;
+import lombok.extern.log4j.Log4j2;
 import dev.begell.entities.Employee;
+import dev.begell.utilities.*;
+import java.sql.*;
 
+@Log4j2
 public class EmployeeDAOPostgresImpl implements EmployeeDAO {
     public Employee createEmployee(Employee employee) {
-        return null;
+        try {
+            Connection conn = ConnectionUtil.createConnection();
+            String sql = "insert into employee values(default,?,?)";
+            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, employee.getFirstName());
+            ps.setString(2, employee.getLastName());
+
+            ps.execute();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            rs.next();
+            int generatedId = rs.getInt("employee_id");
+            employee.setEmployeeId(generatedId);
+            return employee;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+            return null;
+        }
     }
 
     public Employee getEmployeeById(int id) {
-        return null;
+        try {
+            Connection conn = ConnectionUtil.createConnection();
+            String sql = "select * from employee where employee_id = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+
+            Employee employee = new Employee();
+            try {
+                if (rs.next()) {    //if there is a first record
+                    employee.setEmployeeId((rs.getInt("employee_id")));
+                    employee.setFirstName((rs.getString("first_name")));
+                    employee.setLastName((rs.getString("last_name")));
+                }
+                else
+                    throw new ResourceNotFoundException(id);
+            } catch (ResourceNotFoundException e) {
+                e.printStackTrace();
+                log.error(e.getMessage());
+                return null;
+            }
+            return employee;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+            return null;
+        }
     }
 
     public Employee updateEmployee(Employee employee) {
-        return null;
+        try {
+            Connection conn = ConnectionUtil.createConnection();
+            String sql = "update employee set first_name = ?, last_name = ? where employee_id = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, employee.getFirstName());
+            ps.setString(2, employee.getLastName());
+            ps.setInt(3, employee.getEmployeeId());
+            int rowsUpdated = ps.executeUpdate();
+
+            try {
+                if(rowsUpdated == 0){
+                    throw new ResourceNotFoundException(employee.getEmployeeId());
+                }
+            }catch(ResourceNotFoundException e){
+                e.printStackTrace();
+                log.error(e.getMessage());
+                return null;
+            }
+
+            return employee;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+            return null;
+        }
     }
 
     public boolean deleteEmployeeById(int id) {
-        return false;
+        try {
+            Connection conn = ConnectionUtil.createConnection();
+            String sql = "delete from employee where employee_id = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.execute();
+            return  true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+            return false;
+        }
     }
 }
---
+++ src/test/java/daotests/EmployeeDAOTests.java
@@ -0,0 +1,57 @@
+package daotests;
+
+import dev.begell.data.EmployeeDAO;
+import dev.begell.data.EmployeeDAOPostgresImpl;
+import dev.begell.entities.Employee;
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class) //for running ordered test cases
+public class EmployeeDAOTests {
+
+    static EmployeeDAO employeeDAO = new EmployeeDAOPostgresImpl();
+    static Employee testEmployee = null;
+
+    @Test
+    @Order(1) // JUnit does not run tests in order by default
+    void create_employee_test(){
+        // An entity that is created but not yet saved should have an id of 0
+        // once saved that Employee should be some non-zero value
+        Employee frank = new Employee(0,"Franklin","Redhot");
+        Employee savedEmployee = employeeDAO.createEmployee(frank);
+        EmployeeDAOTests.testEmployee = savedEmployee;// I have a Employee I can use in other tests
+        Assertions.assertNotEquals(0,savedEmployee.getEmployeeId());
+    }
+
+    @Test
+    @Order(2)
+    void get_Employee_by_id(){
+        Employee retrievedEmployee  = employeeDAO.getEmployeeById(testEmployee.getEmployeeId());// use the id generated from the previous test
+        // I have avoided hard coded values and made my test repeatable
+        Assertions.assertEquals("Franklin", retrievedEmployee.getFirstName());
+        Assertions.assertEquals("Redhot", retrievedEmployee.getLastName());
+    }
+
+    @Test
+    @Order(3)
+    void update_Employee(){
+        EmployeeDAOTests.testEmployee.setFirstName("Tommy");
+        EmployeeDAOTests.testEmployee.setLastName("Tabasco");
+        employeeDAO.updateEmployee(testEmployee);// the new title should be saved to the database
+        Employee retrievedEmployee = employeeDAO.getEmployeeById(testEmployee.getEmployeeId());
+        Assertions.assertEquals("Tommy",retrievedEmployee.getFirstName());
+        Assertions.assertEquals("Tabasco", retrievedEmployee.getLastName());
+    }
+
+    @Test
+    @Order(4)// to not run something
+    void delete_Employee(){
+        boolean result = employeeDAO.deleteEmployeeById(testEmployee.getEmployeeId()); // true if successful
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    @Order(5)
+    void get_non_existent_Employee(){
+        Assertions.assertNull(employeeDAO.getEmployeeById(10000));
+    }
+}
