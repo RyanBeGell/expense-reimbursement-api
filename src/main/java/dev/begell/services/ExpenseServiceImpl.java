5e5b41eb147df9bb300da524fbf0cd56f5582b4a
RyanBeGell <Ryanbegell@outlook.com>
2022-04-23 18:52:51 -0400

    Method implementation added

--- src/main/java/dev/begell/services/ExpenseServiceImpl.java
+++ src/main/java/dev/begell/services/ExpenseServiceImpl.java
@@ -1,53 +1,78 @@
 package dev.begell.services;
 
+import dev.begell.data.ExpenseDAO;
 import dev.begell.entities.Expense;
 
 import java.util.List;
 
 public class ExpenseServiceImpl implements ExpenseService {
 
+    private final ExpenseDAO expenseDAO;
+
+    //dependency injection
+    public ExpenseServiceImpl(ExpenseDAO expenseDAO){
+        this.expenseDAO = expenseDAO;
+    }
+
     @Override
     public Expense registerNewExpense(Expense expense) {
-        return null;
+        return this.expenseDAO.createExpense(expense);
     }
 
     @Override
     public Expense retrieveExpenseByExpenseId(int id) {
-        return null;
+        return expenseDAO.getExpenseById(id);
     }
 
     @Override
-    public Expense approveExpense(Expense expense) {
-        return null;
+    public boolean approveExpense(Expense expense) {
+        expense.setApproved(true);
+        return true;
     }
 
     @Override
-    public Expense denyExpense(Expense expense) {
-        return null;
+    public boolean denyExpense(Expense expense) {
+        expense.setApproved(false);
+        return true;
     }
 
     @Override
-    public Expense deleteExpenseById(int id) {
-        return null;
+    public Boolean deleteExpenseById(int id) {
+        if(this.expenseDAO.getExpenseById(id) != null) {
+            return this.expenseDAO.deleteExpenseById(id);
+        } else {
+            return false;
+        }
     }
 
     @Override
     public Expense replaceExpense(Expense expense) {
-        return null;
+        return this.expenseDAO.updateExpense(expense);
     }
 
     @Override
     public List<Expense> getAllExpensesByEmployeeId(int id) {
-        return null;
+        List<Expense> expenses = expenseDAO.getAllExpenses();
+        expenses.forEach(expense ->{
+            if(expense.getExpenseId() != id){
+                expenses.remove(expense);
+            }
+        });
+        return expenses;
     }
 
     @Override
     public List<Expense> getAllExpenses() {
-        return null;
+        return expenseDAO.getAllExpenses();
     }
 
     @Override
     public List<Expense> getAllPendingExpenses() {
-        return null;
+        List<Expense> expenses = expenseDAO.getAllExpenses();
+        expenses.forEach(expense -> {
+            if(!expense.isApproved())
+                expenses.remove(expense);
+        });
+        return expenses;
     }
 }
